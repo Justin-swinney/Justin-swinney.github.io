63bd54e8e20cd3829cfbfcc47d014172e9dee37f
Justin Swinney <140655243+Justin-swinney@users.noreply.github.com>
2024-04-20 15:36:59 -0400

    data load fix v.1

--- scripts/data.js
+++ scripts/data.js
@@ -19,14 +19,15 @@ async function insertContent(elementId, filePath) {
 document.addEventListener('DOMContentLoaded', async function() {
     const isIndexPage = window.location.pathname.includes('index');
     const isCapstonePage = window.location.pathname.includes('capstone');
+    await insertContent('intro', '/assets/data/index/intro.txt');
+    console.log("Loaded Index intro data successfully");
     try {
         if (isIndexPage) {
             console.log('Index Page Hit')
             /* Index page. */
             // Intro
-            insertContent('intro', '/assets/data/index/intro.txt').then(r => {
-                console.log("Loaded TEST Index intro data successfully");
-            });
+            await insertContent('intro', '/assets/data/index/intro.txt');
+            console.log("Loaded Index intro data successfully");
 
             // Building a better me
             await insertContent('building-a-better-me', '/assets/data/index/building-better-me.txt');
